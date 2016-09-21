92dde4911aa38a18aa1203c38cc2c4cba4e18efa
760766307 <jiangedabaojian@qq.com>
2016-09-21 09:32:41 +0800

    9.21
    版本

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  鑫考报修
+//
+//  Created by 杨剑 on 16/9/21.
+//  Copyright © 2016年 贱贱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
